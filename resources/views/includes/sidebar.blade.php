75558821bc7c49d6200257c5d2f601c83bcc6c2f
aldididi <aldyh45@gmail.com>
2020-11-09 21:02:52 +0700

    adding transaction:model,index,detail,updt, chng stat, delete and stats

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -24,10 +24,10 @@
 
                     <li class="menu-title">Transaksi</li><!-- /.menu-title -->
                     <li class="">
-                        <a href="#"> <i class="menu-icon fa fa-list"></i>Lihat Transaksi</a>
+                    <a href="{{route('transactions.index')}}"> <i class="menu-icon fa fa-list"></i>Lihat Transaksi</a>
                     </li>
                 </ul>
-            </div><!-- /.navbar-collapse -->
+            </div>
         </nav>
     </aside>
     <!-- /#left-panel -->
